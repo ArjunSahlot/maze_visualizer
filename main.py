f04c0bca64ed5441dceaf0925759234f131f2af0
ArjunSahlot <iarjun.sahlot@gmail.com>
2021-01-21 22:10:22 -0800

    maze and cell

--- main.py
+++ main.py
@@ -11,16 +11,23 @@ pygame.display.set_caption("Maze Generator")
 def main(window):
     pygame.init()
     clock = pygame.time.Clock()
-    maze = Maze()
+    maze = Maze(0, 200, WIDTH, HEIGHT-200)
 
     while True:
         clock.tick(FPS)
-        window.fill(WHITE)
+        window.fill(GREY)
         events = pygame.event.get()
+        keys = pygame.key.get_pressed()
+        ctrl_pressed = keys[pygame.K_LCTRL] or keys[pygame.K_RCTRL]
+        maze.update(window, events)
         for event in events:
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_q and ctrl_pressed:
+                    pygame.quit()
+                    return
         pygame.display.update()
 
 
--- maze.py
+++ maze.py
@@ -1,5 +1,33 @@
 import pygame
+from random import randint
 
 
 class Maze:
-    pass
+    def __init__(self, x, y, width, height):
+        self.x, self.y, self.width, self.height = x, y, width, height
+        self.rows, self.cols = height // 15, width // 15
+        self.cells = [[Cell(row, col, 15) for col in range(self.cols)] for row in range(self.rows)]
+    
+    def update(self, window, events):
+        self.draw(window)
+    
+    def draw(self, window):
+        for row in self.cells:
+            for cell in row:
+                cell.draw(window, self.x, self.y)
+
+
+class Cell:
+    colors = {
+        True: (255,) * 3,
+        False: (0,) * 3
+    }
+
+    def __init__(self, row, col, width):
+        self.row, self.col, self.width = row, col, width
+        self.state = randint(1, 2) == 1
+    
+    def draw(self, window, x_off, y_off):
+        x = x_off + self.col*self.width
+        y = y_off + self.row*self.width
+        pygame.draw.rect(window, self.colors[self.state], (x, y, self.width, self.width))
