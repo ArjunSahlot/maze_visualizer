5c29564c6c3312f3b3c2a7624f4e09939752981b
ArjunSahlot <iarjun.sahlot@gmail.com>
2021-01-25 22:19:22 -0800

    few element tweaks

--- interface.py
+++ interface.py
@@ -6,11 +6,11 @@ from constants import *
 class Interface:
     def __init__(self, height):
         self.height = height
-        self.gen = Button(WIDTH - 150 - 5, 5, 150, 60, "Generate", 5)
+        self.gen = Button(WIDTH - 250 - 5, 5, 250, 60, "Generate", 5)
         self.find = Button(self.gen.x, self.gen.y + self.gen.height + 5, self.gen.width, 60, "Find Path", self.gen.border)
         self.speed = Slider(self.find.x, self.find.y + self.find.height + 10, self.find.width, 50, label="Speed")
-        self.find_drop = Dropdown((self.gen.x - 10 - 200, 5), (200, 50), (200, 140), BLACK, "Path Finding Algs")
-        self.gen_drop = Dropdown((self.find_drop.loc[0] - 10 - 200, 5), (200, 50), (200, 140), BLACK, "Maze Gen Algs")
+        self.find_drop = Dropdown((self.gen.x - 20 - 300, 5), (300, 50), (300, 140), BLACK, "Path Finding Algs", color=(255,)*3, border_col=(255,)*3)
+        self.gen_drop = Dropdown((self.find_drop.loc[0] - 10 - self.find_drop.size[0], 5), self.find_drop.size, self.find_drop.pop_size, BLACK, "Maze Gen Algs", color=self.find_drop.color, border_col=self.find_drop.border_col)
 
     def update(self, window, events):
         self.gen.update(window, events)
