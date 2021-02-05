d2e18c752b70ef2348bbe62e26bd8538ccf7fb00
ArjunSahlot <iarjun.sahlot@gmail.com>
2021-02-04 22:57:23 -0800

    check before updating color

--- maze.py
+++ maze.py
@@ -118,7 +118,8 @@ class Maze:
             if not self.active:
                 clock.tick(speed.value*100)
                 curr = open.pop()
-                curr.open()
+                if curr not in ("start", "end"):
+                    curr.open()
                 for n in self.get_pathfind_neighbors(curr):
                     if n not in visited:
                         if n == self.end:
