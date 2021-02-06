ed0a94b4169600c0418e9e94ba7fce174101e2b3
ArjunSahlot <iarjun.sahlot@gmail.com>
2021-02-05 18:58:06 -0800

    improved dijkstras

--- maze.py
+++ maze.py
@@ -410,19 +410,21 @@ class Maze:
         while not open.empty():
             if not self.active:
                 clock.tick(speed.value*100)
-                if (curr := open.get()[2]) == "end":
-                    self.reconstruct(path, speed)
-                    return
+                curr = open.get()[2]
 
                 temp_g = g_score[curr] + 1
-                for neighbor in self.get_pathfind_neighbors(curr):
-                    if temp_g < g_score[neighbor]:
-                        path[neighbor] = curr
-                        g_score[neighbor] = temp_g
-                        if not any(neighbor == item[2] for item in open.queue):
+                for n in self.get_pathfind_neighbors(curr):
+                    if n == "end":
+                        path[n] = curr
+                        self.reconstruct(path, speed)
+                        return
+                    if temp_g < g_score[n]:
+                        path[n] = curr
+                        g_score[n] = temp_g
+                        if not any(n == item[2] for item in open.queue):
                             count += 1
-                            open.put((g_score[neighbor], count, neighbor))
-                            neighbor.close()
+                            open.put((g_score[n], count, n))
+                            n.close()
 
                 if curr != self.start:
                     curr.open()
