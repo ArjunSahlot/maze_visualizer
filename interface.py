c5b3677d69532b67d8ad3297cd7ed2a321d49fd9
ArjunSahlot <iarjun.sahlot@gmail.com>
2021-02-12 18:23:29 -0800

    trailing

--- interface.py
+++ interface.py
@@ -134,7 +134,7 @@ class Interface:
         pygame.draw.line(window, (255,)*3, (self.cell_size.x, self.height - 5 - 50), (self.cell_size.x, self.height), 5)
         pygame.draw.line(window, (255,)*3, (self.cell_size.x + self.cell_size.width/2, self.height - 5 - 50), (self.cell_size.x + self.cell_size.width/2, self.height), 5)
         pygame.draw.line(window, (255,)*3, (self.cell_size.x + self.cell_size.width, self.height - 5 - 50), (self.cell_size.x + self.cell_size.width, self.height), 5)
-        
+
 
         if self.gen.clicked(events):
             if (selected := self.gen_drop.get_selection()) == self.maze_drop_text:
